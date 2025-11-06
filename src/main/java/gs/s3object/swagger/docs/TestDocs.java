c430c52a77df3225dc71a9334a08e6c259156719
choi4624 <choi4624@gmail.com>
2025-11-06 16:35:40 +0900

    swagger 설정 서로 안맞아서 제거

---
+++ src/main/java/gs/s3object/swagger/docs/TestDocs.java
@@ -0,0 +1,15 @@
+//package gs.s3object.swagger.docs;
+//
+//
+//
+//@Tag(name = "SpringTest", description = "Test와 관련된 컨틀롤러로 접근 가능한 api")
+//public interface TestDocs {
+////    @Operation(summary = "BOM 기초 정보 확인", description = "jpa로 돌아가는 정보 확인")
+////    @ApiResponses(value = {
+////            @ApiResponse(responseCode = "200", description = "성공"),
+////            @ApiResponse(responseCode = "404", description = "NOT found 404"),
+////            @ApiResponse(responseCode = "500", description = "내부 로직 오류")
+////    })
+////    public ResponseEntity<SpringTestDTO> getSpringTestDTOs(@PathVariable String partCode,
+////                                                                       @PathVariable(required = false) Integer seq);
+//}
