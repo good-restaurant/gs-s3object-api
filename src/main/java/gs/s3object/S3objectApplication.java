10647fc212db8569287a387e07ecbe31f70187bb
choi4624 <choi4624@gmail.com>
2025-11-06 15:04:53 +0900

    spring initalizer

---
+++ src/main/java/gs/s3object/S3objectApplication.java
@@ -0,0 +1,13 @@
+package gs.s3object;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3objectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3objectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gs/s3object/S3objectApplicationTests.java
@@ -0,0 +1,13 @@
+package gs.s3object;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3objectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
